720558895832b02952c9263090e79289add6c0dc
Zeno Rocha <hi@zenorocha.com>
2015-04-06 12:15:11 -0700

    Uses option params from metal

--- tasks/build.js
+++ tasks/build.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
+var path = require('path');
 var uglify = require('gulp-uglify');
+var runSequence = require('run-sequence');
+var options = require('metaljs/tasks/lib/options')();
 
-gulp.task('build', ['build:globals'], function() {
-  return gulp.src(['build/boilerplate.js'])
-    .pipe(uglify())
-    .pipe(gulp.dest('build'));
+gulp.task('build:min', function() {
+  return gulp.src(path.join(options.buildDest, 'boilerplate.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(options.buildDest));
+});
+
+gulp.task('build', function(cb) {
+  runSequence('build:globals', 'build:min', cb);
 });
