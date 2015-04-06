4e3341967150fb4806ed9ec50d1c7dd52ed6a6ae
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2015-04-06 16:52:02 -0300

    Renames before minify file

--- tasks/build.js
+++ tasks/build.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var path = require('path');
+var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var runSequence = require('run-sequence');
 var options = require('metaljs/tasks/lib/options')();
@@ -7,6 +8,9 @@ var options = require('metaljs/tasks/lib/options')();
 gulp.task('build:min', function() {
   return gulp.src(path.join(options.buildDest, 'boilerplate.js'))
       .pipe(uglify())
+      .pipe(rename(function (path) {
+        path.basename += '-min';
+      }))
       .pipe(gulp.dest(options.buildDest));
 });
 
