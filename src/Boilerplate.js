51a58046e1ccf66659520904df3cf7372d6113e9
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2015-04-06 17:27:56 -0300

    Fix indent style

--- src/Boilerplate.js
+++ src/Boilerplate.js
@@ -7,101 +7,101 @@ import './Boilerplate.soy.js';
  * Boilerplate component.
  */
 class Boilerplate extends SoyComponent {
-  /**
-   * @inheritDoc
-   */
-  constructor(opt_config) {
-    super(opt_config);
-  }
+	/**
+	 * @inheritDoc
+	 */
+	constructor(opt_config) {
+		super(opt_config);
+	}
 
-  /**
-   * Lifecycle. Creation phase of the component happens once after the
-   * component is instantiated, therefore its the initial phase of the
-   * component Lifecycle. Be conscious about actions performed in this phase
-   * to not compromise instantiation time with operations that can be
-   * postponed to further phases. It's recommended to bind component custom
-   * events in this phase, in contrast to DOM events that must be bind on
-   * attach phase.
-   */
-  created() {
-  }
+	/**
+	 * Lifecycle. Creation phase of the component happens once after the
+	 * component is instantiated, therefore its the initial phase of the
+	 * component Lifecycle. Be conscious about actions performed in this phase
+	 * to not compromise instantiation time with operations that can be
+	 * postponed to further phases. It's recommended to bind component custom
+	 * events in this phase, in contrast to DOM events that must be bind on
+	 * attach phase.
+	 */
+	created() {
+	}
 
-  /**
-   * Lifecycle. When attached, the component element is appended to the DOM
-   * and any other action to be performed must be implemented in this method,
-   * such as, binding DOM events. A component can be re-attached multiple
-   * times, therefore the undo behavior for any action performed in this phase
-   * must be implemented on the detach phase.
-   */
-  attached() {
-  }
+	/**
+	 * Lifecycle. When attached, the component element is appended to the DOM
+	 * and any other action to be performed must be implemented in this method,
+	 * such as, binding DOM events. A component can be re-attached multiple
+	 * times, therefore the undo behavior for any action performed in this phase
+	 * must be implemented on the detach phase.
+	 */
+	attached() {
+	}
 
-  /**
-   * Lifecycle. When detached, the component element is removed from the DOM
-   * and any other action to be performed must be implemented in this method,
-   * such as, unbinding DOM events. A component can be detached multiple
-   * times, therefore the undo behavior for any action performed in this phase
-   * must be implemented on the attach phase.
-   */
-  detached() {
-  }
+	/**
+	 * Lifecycle. When detached, the component element is removed from the DOM
+	 * and any other action to be performed must be implemented in this method,
+	 * such as, unbinding DOM events. A component can be detached multiple
+	 * times, therefore the undo behavior for any action performed in this phase
+	 * must be implemented on the attach phase.
+	 */
+	detached() {
+	}
 
-  /**
-   * Sets component visibility to false. Invoked by soy template event binding.
-   */
-  close() {
-    this.visible = false;
-  }
+	/**
+	 * Sets component visibility to false. Invoked by soy template event binding.
+	 */
+	close() {
+		this.visible = false;
+	}
 
-  /**
-   * Invokes when visible attribute changes to synchronize the visible state.
-   * @param {boolean} visible
-   */
-  syncVisible(visible) {
-    this.element.style.display = visible ? null : 'none';
-  }
+	/**
+	 * Invokes when visible attribute changes to synchronize the visible state.
+	 * @param {boolean} visible
+	 */
+	syncVisible(visible) {
+		this.element.style.display = visible ? null : 'none';
+	}
 }
 
 Boilerplate.ATTRS = {
-  /**
-   * Attribute that holds the body content.
-   * @type {string}
-   * @default ''
-   */
-  bodyContent: {
-    validator: core.isString,
-    value: ''
-  },
+	/**
+	 * Attribute that holds the body content.
+	 * @type {string}
+	 * @default ''
+	 */
+	bodyContent: {
+		validator: core.isString,
+		value: ''
+	},
 
-  /**
-   * Attribute that holds the header content.
-   * @type {string}
-   * @default ''
-   */
-  headerContent: {
-    validator: core.isString,
-    value: ''
-  },
+	/**
+	 * Attribute that holds the header content.
+	 * @type {string}
+	 * @default ''
+	 */
+	headerContent: {
+		validator: core.isString,
+		value: ''
+	},
 
-  /**
-   * Attribute that holds the footer content.
-   * @type {string}
-   * @default ''
-   */
-  footerContent: {
-    validator: core.isString,
-    value: ''
-  },
+	/**
+	 * Attribute that holds the footer content.
+	 * @type {string}
+	 * @default ''
+	 */
+	footerContent: {
+		validator: core.isString,
+		value: ''
+	},
 
-  /**
-   * Attribute that holds the visibility.
-   * @type {boolean}
-   * @default true
-   */
-  visible: {
-    validator: core.isBoolean,
-    value: true
-  }
+	/**
+	 * Attribute that holds the visibility.
+	 * @type {boolean}
+	 * @default true
+	 */
+	visible: {
+		validator: core.isBoolean,
+		value: true
+	}
 };
 
 /**
